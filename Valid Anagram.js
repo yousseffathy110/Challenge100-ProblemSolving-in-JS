e00da39bb4276cc557608cd84965f6b71217f089
yousef-fathy <yf1102003@gmail.com>
2024-03-12 23:15:44 +0200

    Done

--- Valid Anagram.js
+++ Valid Anagram.js
@@ -1,1 +1,6 @@
-const isAnagram = (s, t) => {};
+const isAnagram = (s, t) => {
+  let arrone = s.split("").sort().join("");
+  let arrtwo = t.split("").sort().join("");
+  if (arrone == arrtwo) return true;
+  else return false;
+};
