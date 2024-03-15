1506b15af3d1d7a972a3208f1beea09e07128ccb
yousef-fathy <yf1102003@gmail.com>
2024-03-15 21:52:42 +0200

    Done

---
+++ jewels-and-Stones.js
@@ -0,0 +1,19 @@
+// using sets
+const numJewelsInStones = (jewels, stones) => {
+  let count = 0,
+    mySet = new Set();
+  for (let i = 0; i < jewels.length; i++) mySet.add(jewels[i]);
+  for (let j = 0; j < stones.length; j++) if (mySet.has(stones[j])) count++;
+  return count;
+};
+
+// using hashmap
+const numJewelsInStones = (jewels, stones) => {
+  const jewelMap = {};
+  let count = 0;
+  for (const jewel of jewels) jewelMap[jewel] = true;
+
+  for (const stone of stones) if (jewelMap[stone]) count++;
+
+  return count;
+};
