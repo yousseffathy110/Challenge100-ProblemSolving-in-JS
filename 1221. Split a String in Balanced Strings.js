7d2c18b883a2a5f1acdab5cecb47d880a23b1f0c
yousef-fathy <yf1102003@gmail.com>
2024-03-19 21:38:19 +0200

    Done

--- 1221. Split a String in Balanced Strings.js
+++ 1221. Split a String in Balanced Strings.js
@@ -1,3 +1,42 @@
+//o(n^2)
+const balancedStringSplit = (s) => {
+  let count = 0,
+    str = "",
+    size = s.length;
+  for (let i = 0; i < size; i++) {
+    str += s[i];
+    if (str.length % 2 == 0) {
+      let r = 0,
+        l = 0;
+      for (let j = 0; j < str.length; j++) {
+        str[j] == "R" ? r++ : l++;
+      }
+      if (r == l) {
+        count++;
+        str = "";
+      }
+    }
+  }
+  return count;
+};
+
+// second solution
+//o(n)
 const balancedStringSplit = (s) => {
   let count = 0;
+  let balance = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "R") {
+      balance++;
+    } else {
+      balance--;
+    }
+
+    if (balance === 0) {
+      count++;
+    }
+  }
+
+  return count;
 };
