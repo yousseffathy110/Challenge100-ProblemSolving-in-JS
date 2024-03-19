2095a0094adcf257bfbe37e529b175b3c921a6b1
yousef-fathy <yf1102003@gmail.com>
2024-03-19 22:48:00 +0200

    Done

--- 2108. Find First Palindromic String in the Array.js
+++ 2108. Find First Palindromic String in the Array.js
@@ -1,3 +1,18 @@
-const firstPalindrome = (words) =>{
-    
-}
+//first solution
+const firstPalindrome = (words) => {
+  for (let i = 0; i < words.length; i++) {
+    let count = 0;
+    for (let j = 0; j < Math.floor(words[i].length / 2); j++) {
+      if (words[i][j] == words[i][words[i].length - 1 - j]) count++;
+    }
+    if (count === Math.floor(words[i].length / 2)) return words[i];
+  }
+  return "";
+};
+
+//second solution using builtin methods
+const firstPalindrome = (words) => {
+  for (let i = 0; i < words.length; i++)
+    if (words[i] === words[i].split("").reverse().join("")) return words[i];
+  return "";
+};
