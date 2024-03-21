fae9baf8bf8e79153f9f545300a1556f9d6cf769
yousef-fathy <yf1102003@gmail.com>
2024-03-22 00:59:04 +0200

    Done

--- 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.js
+++ 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.js
@@ -1,3 +1,16 @@
+//solutio one
 const minPartitions = (n) => {
-    
+  let max = 0,
+    arr = n.split("");
+  for (let i = 0; i < arr.length; i++) {
+    let digit = parseInt(arr[i]);
+    if (max < digit) max = digit;
+  }
+  return max;
 };
+// solution 2
+const minPartitions = (n) => {
+  Math.max(...n.split("").map(Number));
+};
+
+console.log(minPartitions("27346209830709182346"));
