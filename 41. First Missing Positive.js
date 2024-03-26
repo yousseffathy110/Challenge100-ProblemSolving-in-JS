375200f3d634ef5a16ec8ec1d2d0f9bb8e7d3058
yousef-fathy <yf1102003@gmail.com>
2024-03-26 06:41:36 +0200

    Done

--- 41. First Missing Positive.js
+++ 41. First Missing Positive.js
@@ -1,3 +1,23 @@
-const firstMissingPositive = nums =>{
-    
-}
+//my dirst solution
+
+const firstMissingPositive = (nums) => {
+  let mySet = new Set(),
+    i = 1,
+    positiveNums = nums
+      .filter((x) => {
+        return x > 0;
+      })
+      .sort((a, b) => a - b);
+  positiveNums.forEach((element) => {
+    mySet.add(element);
+  });
+  while (i > 0) {
+    if (!mySet.has(i)) {
+      return i;
+      break;
+    }
+    i++;
+  }
+};
+
+console.log(firstMissingPositive([7, 8, 9, 11, 12]));
