c4b3a06d8bd3b80511beba119bd5dd6c8ff4fd21
yousef-fathy <yf1102003@gmail.com>
2024-03-27 19:19:41 +0200

    Done

---
+++ 713. Subarray Product Less Than K.js
@@ -0,0 +1,12 @@
+const numSubarrayProductLessThanK = (nums, k) => {
+  if (k <= 1) return 0;
+  let ans = 0,
+    prod = 1;
+  for (let l = 0, r = 0; r < nums.length; ++r) {
+    prod *= nums[r];
+    while (prod >= k) prod /= nums[l++];
+    ans += r - l + 1;
+  }
+  
+  return ans;
+};
